0dc4f0f247de3dfacbc0c21c04ddf8830d87e2c9
ProtonGamerHun1238 <68368558+ProtonGamerHun1238@users.noreply.github.com>
2020-07-16 09:38:16 +0200

    500ms

--- main.ts
+++ main.ts
@@ -4,13 +4,20 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.AB, function () {
     control.reset()
 })
+input.onButtonPressed(Button.B, function () {
+    while (asd == 1) {
+        hozzaeres = 0
+    }
+})
+let asd = 0
 let hozzaeres = 0
 hozzaeres = 0
+asd = 1
 basic.forever(function () {
     if (input.pinIsPressed(TouchPin.P1)) {
         hozzaeres += 1
         basic.showIcon(IconNames.Skull)
-        basic.pause(1000)
+        basic.pause(500)
     } else if (!(input.pinIsPressed(TouchPin.P1))) {
         basic.showIcon(IconNames.Happy)
     }
