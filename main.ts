ad33a3e8f5038028f5d8e470f4c7e337cdfd69ef
ProtonGamerHun1238 <68368558+ProtonGamerHun1238@users.noreply.github.com>
2020-07-16 09:33:00 +0200

    Added Wait function and variables

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(hozzaeres)
+})
+input.onButtonPressed(Button.AB, function () {
+    control.reset()
+})
+let hozzaeres = 0
+hozzaeres = 0
 basic.forever(function () {
-	
+    if (input.pinIsPressed(TouchPin.P1)) {
+        hozzaeres += 1
+        basic.showIcon(IconNames.Skull)
+        basic.pause(1000)
+    } else if (!(input.pinIsPressed(TouchPin.P1))) {
+        basic.showIcon(IconNames.Happy)
+    }
 })
